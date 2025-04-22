6cd6c54d8ffd0dfa44d7c4e265937aae1a1fc8cd
RakeshRevashetti <rakeshyatish@gmail.com>
2025-04-22 15:12:09 +0530

    cmd python script

---
+++ commandline_args/cmd.py
@@ -0,0 +1,13 @@
+
+import sys
+
+
+my_script_name = sys.argv[0]
+first_argument = sys.argv[1]
+second_argument = sys.argv[2]
+third_argument = sys.argv[3]
+
+print("The python script name is:", my_script_name)
+print("The first argument is:", first_argument)
+print("The second argument is:", second_argument)
+print("The third argument is:", third_argument) 
