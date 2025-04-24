49b0c304ba97102ecaec6db8822612509ecc3697
RakeshRevashetti <rakeshyatish@gmail.com>
2025-04-24 07:03:20 +0530

    string, list and tuples

---
+++ data_types/sequence_data_type.py
@@ -0,0 +1,23 @@
+#-------------------------------------1: String -----------------------------------
+
+my_string = "Rakesh Revashetti"
+print(my_string)
+
+#-------------------------------------2: List -------------------------------------
+
+my_list = ["Rakesh", "Revashetti", 9886029488, 28]
+for i in my_list:
+ print(i)
+
+my_list.append("Devops_Engineer")
+print(my_list)
+
+my_list.remove(28)
+print(my_list)
+
+#-------------------------------------3: Tuple --------------------------------------
+
+my_tuple = ("Rakesh", "Revashetti", 9886029488, 28)
+for i in my_tuple:
+ print(i)
+#Note: Immutability
