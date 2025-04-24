972e8417995e36a8c03ad9150e6d1bc31b96c0e3
RakeshRevashetti <rakeshyatish@gmail.com>
2025-04-24 06:28:58 +0530

    Output alterations

--- file_operations/exception_handling.py
+++ file_operations/exception_handling.py
@@ -3,18 +3,20 @@ import os
 
 def list_files_in_folder(folder_name):
     try:
+        
          files = os.listdir(folder_name)
          if not files:
             print(f"The folder {folder_name} is empty.")
             return None
          else:
-            print("\nThe folder " +folder_name+  " contains the following files:")
-            for i in files:
+             print("\nThe folder contains the following files:")
+             print("-----------------------------------------")
+             for i in files:
             
-             print(i)
+              print(i)
          return files
     except FileNotFoundError:
-        print("\nError: The folder " +folder_name+ " does not exist.")
+        print("The folder " +folder_name+ " does not exist.")
         return None 
         
 
@@ -23,7 +25,7 @@ def main():
    folder_name = input("Enter the folder name: ").split() 
    for folder_path in folder_name:
        print("\nThe folder name is: ", folder_path)
-
+       print("-----------------------------------------")
        list_files_in_folder(folder_path)
                 
 if __name__ == "__main__":
